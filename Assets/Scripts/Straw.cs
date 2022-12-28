fee3216e981ea1838c13b87f3daf938037856e7a
Hsiang <q224399377@gmail.com>
2022-12-28 10:14:06 +0800

    test

--- Assets/Scripts/Straw.cs
+++ Assets/Scripts/Straw.cs
@@ -8,16 +8,8 @@ public class Straw : MonoBehaviour
     public float speed;
     public float speedBack;
     public float distanceBetween;
-
-    
-
     public Transform pos;
-    //public Transform cam;
-    //public float xCoordinate, yCoordinate, zCoordinate;
-
     private float distance;
-    //private bool tooClose = false;
-
     public static bool isInStraw;
 
     // Start is called before the first frame update
@@ -34,11 +26,7 @@ public class Straw : MonoBehaviour
         Vector2 direction = player.transform.position - transform.position;
         direction.Normalize();
 
-        //if (distance < distanceBetween && (transform.position.y >= player.transform.position.y + 5)) {
-
         if (distance < distanceBetween && !isInStraw) {
-            //Debug.Log(isInStraw);
-            //transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
             if(transform.position.y >= player.transform.position.y + 5) {
                 transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
             } else {
@@ -46,38 +34,10 @@ public class Straw : MonoBehaviour
             }
         } 
         else {
-            //Debug.Log(isInStraw);
             transform.position = Vector2.MoveTowards(this.transform.position, pos.position, speedBack * Time.deltaTime);
         } 
 
         
     }
 
-    // void OnTriggerEnter2D (Collider2D other) {
-    //     if (other.gameObject.CompareTag("Player")) {
-    //         isInStraw = true;
-    //         Debug.Log("in straw");
-    //     }
-    // }
-
-    // void OnTriggerExit2D (Collider2D other) {
-    //     if (other.gameObject.CompareTag("Player")) {
-    //         isInStraw = false;
-    //         Debug.Log("not in straw");
-    //     }
-    // }
-
-    // void OnTriggerEnter2D (Collider2D other) {
-    //     if (other.gameObject.CompareTag("AroundPlayer")) {
-    //         Debug.Log("tooClose");
-    //         tooClose = true;
-    //     }
-        
-    // }
-    // void OnTriggerExit2D (Collider2D other) {
-    //     if (other.gameObject.CompareTag("AroundPlayer")) {
-    //         Debug.Log("notTooClose");
-    //         tooClose = false;
-    //     }
-    // }
 }
