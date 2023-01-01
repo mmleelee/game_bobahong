22cd9ca60f9ee87b5210a7d0bbbbbc350678c860
VickyChang <a0197391@gmail.com>
2023-01-01 13:50:02 +0800

    add choose level and play button function

--- Assets/Scripts/ChooseScene/ChoosePage.cs
+++ Assets/Scripts/ChooseScene/ChoosePage.cs
@@ -10,4 +10,9 @@ public class ChoosePage : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
 
+    public void GoToNext()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
 }
