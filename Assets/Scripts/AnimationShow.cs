eb8c502c94e1fb4bc61d7af116c37757ce79e324
VickyChang <a0197391@gmail.com>
2023-01-02 12:26:32 +0800

    add animation to game

---
+++ Assets/Scripts/AnimationShow.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AnimationShow : MonoBehaviour
+{
+    public GameObject skipMenu;
+
+
+    public void GoToMainPage()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        Time.timeScale = 1f;
+    }
+
+    public void OpenSkip()
+    {
+        skipMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void Close()
+    {
+        skipMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+}
