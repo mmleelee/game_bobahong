849ef424c7ce3a0d77dd97bedf07a385fb142157
Hsiang <q224399377@gmail.com>
2023-01-02 17:26:04 +0800

    edit straw

--- Assets/Scripts/Straw.cs
+++ Assets/Scripts/Straw.cs
@@ -26,7 +26,18 @@ public class Straw : MonoBehaviour
         Vector2 direction = player.transform.position - transform.position;
         direction.Normalize();
 
-        if (distance < distanceBetween && !isInStraw && StartingPoint.isStart) {
+        // if (distance < distanceBetween && !isInStraw && StartingPoint.isStart) {
+        //     if(transform.position.y >= player.transform.position.y + 5) {
+        //         transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+        //     } else {
+        //         transform.position = Vector2.MoveTowards(this.transform.position, new Vector2(player.transform.position.x, player.transform.position.y + 5), speed * Time.deltaTime);
+        //     }
+        // } 
+        // else {
+        //     transform.position = Vector2.MoveTowards(this.transform.position, pos.position, speedBack * Time.deltaTime);
+        // } 
+
+        if (ChaseArea.chase && !isInStraw && StartingPoint.isStart) {
             if(transform.position.y >= player.transform.position.y + 5) {
                 transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
             } else {
@@ -36,6 +47,31 @@ public class Straw : MonoBehaviour
         else {
             transform.position = Vector2.MoveTowards(this.transform.position, pos.position, speedBack * Time.deltaTime);
         } 
+
+        // if (StartingPoint.isStart){
+        //     if (distance < distanceBetween && !isInStraw) {
+                
+        //         if (transform.position.y >= player.transform.position.y + 5) {
+        //                 if (ChaseArea.chase) {
+        //                     transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+        //                 } else {
+        //                     transform.position = Vector2.MoveTowards(this.transform.position, new Vector2(pos.position.x + 5, player.transform.position.y), speed * Time.deltaTime);
+        //                 }
+        //         } else {
+        //             if (ChaseArea.chase) {
+        //                 transform.position = Vector2.MoveTowards(this.transform.position, new Vector2(player.transform.position.x, player.transform.position.y + 5), speed * Time.deltaTime);
+        //             } else {
+        //                 transform.position = Vector2.MoveTowards(this.transform.position, new Vector2(pos.position.x + 5, player.transform.position.y + 5), speed * Time.deltaTime);
+        //             }
+                        
+        //         }
+        //     } else {
+        //         transform.position = Vector2.MoveTowards(this.transform.position, pos.position, speedBack * Time.deltaTime);
+        //     } 
+        // } else {
+        //         transform.position = Vector2.MoveTowards(this.transform.position, pos.position, speedBack * Time.deltaTime);
+        // } 
+        
 
         
     }
